981179a2b7dcdefb809b28b2e6a60191e5a421dd
Abduarahman Bazena <abduarahman.bazena@gmail.com>
2020-12-09 00:49:15 +0100

    update with semi final charts

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,21 +1,21 @@
 var _DATA = null;
-$('#sidebarCollapse').on('click', function() {
+$('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
 });
-const dashboard = {
-    init: function() {
+const dashboard =
+{
+    init: function () {
         try {
             _DATA = JSON.parse(localStorage.getItem("_DATA"));
         } catch (error) {
             console.log(error)
         }
     },
-    initDashboardOne: function() {
+    initDashboardOne: function () {
         chartOne = {
             "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-            "title": "Mean child mortality Against Mean Life Expectancy over time",
             "width": 600,
-            "height": 600,
+            "height": 450,
             "data": {
                 "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
                 "format": { "type": "dsv", "delimiter": ";" }
@@ -27,7 +27,8 @@ const dashboard = {
                     "type": "ordinal"
                 }
             },
-            "layer": [{
+            "layer": [
+                {
                     "mark": { "color": "#d33636", "type": "line" },
                     "encoding": {
                         "y": {
@@ -57,14 +58,15 @@ const dashboard = {
         chartTwo = {
             "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
             "width": 600,
-            "height": 600,
+            "height": 450,
             "title": "Mean child mortality Against mean fertility over time",
             "data": {
                 "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
                 "format": { "type": "dsv", "delimiter": ";" }
             },
             "encoding": { "x": { "field": "Year", "timeUnit": "year", "type": "ordinal" } },
-            "layer": [{
+            "layer": [
+                {
                     "mark": { "color": "#d33636", "type": "line", "cornerRadiusEnd": 4 },
                     "encoding": {
                         "y": {
@@ -90,19 +92,97 @@ const dashboard = {
             "resolve": { "scale": { "y": "independent" } }
         }
         vegaEmbed('#chart2', chartTwo);
+
+        chartThree = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "title": "Mean child mortality Against mean fertility in America over time",
+            "width": 500,
+            "height": 250,
+            "data": {
+              "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
+              "format": {"type": "dsv", "delimiter": ";"}
+            },
+            "encoding": {"x": {"field": "Year", "timeUnit": "year", "type": "ordinal"}},
+            "layer": [
+              {
+                "mark": {"color": "#d33636", "type": "bar", "cornerRadiusEnd": 1},
+                "encoding": {
+                  "y": {
+                    "field": "fertility",
+                    "aggregate": "mean",
+                    "type": "quantitative"
+                  }
+                }
+              },
+              {
+                "mark": {"stroke": "#176434", "type": "line"},
+                "encoding": {
+                  "y": {
+                    "field": "population",
+                    "aggregate": "mean",
+                    "type": "quantitative",
+                    "axis": {"titleColor": "#000000"}
+                  }
+                }
+              }
+            ],
+            "transform": [{"filter": {"field": "region", "equal": "South Asia"}}],
+            "resolve": {"scale": {"y": "independent"}}
+          }
+          vegaEmbed('#chart3', chartThree);
+
+
+          chartFour = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "title": "Mean child mortality Against mean fertility in America over time",
+            "width": 500,
+            "height": 250,
+            "data": {
+              "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
+              "format": {"type": "dsv", "delimiter": ";"}
+            },
+            "encoding": {"x": {"field": "Year", "timeUnit": "year", "type": "ordinal"}},
+            "layer": [
+              {
+                "mark": {"color": "#d33636", "type": "bar", "cornerRadiusEnd": 1},
+                "encoding": {
+                  "y": {
+                    "field": "life",
+                    "aggregate": "mean",
+                    "type": "quantitative"
+                  }
+                }
+              },
+              {
+                "mark": {"stroke": "#176434", "type": "line"},
+                "encoding": {
+                  "y": {
+                    "field": "population",
+                    "aggregate": "mean",
+                    "type": "quantitative",
+                    "axis": {"titleColor": "#000000"}
+                  }
+                }
+              }
+            ],
+            "transform": [{"filter": {"field": "region", "equal": "South Asia"}}],
+            "resolve": {"scale": {"y": "independent"}}
+          }
+          vegaEmbed('#chart4', chartFour);
     },
-    initDashboardTwo: function() {
+    initDashboardTwo: function () {
         vlSpec = {
             "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
             "title": "Mean child mortality Against mean fertility in America over time",
             "width": 500,
-            "height": 600,
+            "height": 450,
             "data": {
                 "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
                 "format": { "type": "dsv", "delimiter": ";" }
             },
             "encoding": { "x": { "field": "Year", "timeUnit": "year", "type": "ordinal" } },
-            "layer": [{
+            "layer": [
+                {
                     "mark": { "color": "#d33636", "type": "bar", "cornerRadiusEnd": 4 },
                     "encoding": {
                         "y": {
@@ -135,8 +215,8 @@ const dashboard = {
         cartTwo = {
             "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
             "title": "Mean child mortality Against mean life in Russia over time",
-            "width": 500,
-            "height": 600,
+            "width": 1200,
+            "height": 300,
             "data": {
                 "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
                 "format": { "type": "dsv", "delimiter": ";" }
@@ -148,7 +228,8 @@ const dashboard = {
                     "type": "ordinal"
                 }
             },
-            "layer": [{
+            "layer": [
+                {
                     "mark": { "color": "#d33636", "type": "line" },
                     "encoding": {
                         "y": {
@@ -175,5 +256,46 @@ const dashboard = {
             "resolve": { "scale": { "y": "independent" } }
         }
         vegaEmbed('#chart2', cartTwo);
+
+
+
+        chartThree = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "title": "Mean child mortality Against mean fertility in America over time",
+            "width": 500,
+            "height": 450,
+            "data": {
+                "url": "https://raw.githubusercontent.com/abazena/Lab-CW-2/main/data/gapminder.csv",
+                "format": { "type": "dsv", "delimiter": ";" }
+            },
+            "encoding": { "x": { "field": "Year", "timeUnit": "year", "type": "ordinal" } },
+            "layer": [
+                {
+                    "mark": { "color": "#d33636", "type": "bar", "cornerRadiusEnd": 1 },
+                    "encoding": {
+                        "y": {
+                            "field": "fertility",
+                            "aggregate": "mean",
+                            "type": "quantitative"
+                        }
+                    }
+                },
+                {
+                    "mark": { "stroke": "#176434", "type": "line" },
+                    "encoding": {
+                        "y": {
+                            "field": "gdp",
+                            "aggregate": "mean",
+                            "type": "quantitative",
+                            "axis": { "titleColor": "#000000" }
+                        }
+                    }
+                }
+            ],
+            "transform": [{ "filter": { "field": "region", "equal": "Middle East & North Africa" } }],
+            "resolve": { "scale": { "y": "independent" } }
+        }
+        vegaEmbed('#chart3', chartThree);
+
     }
 }
